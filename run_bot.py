d0a2a11c86dc4002d0fdfb01c99e79d19a2a8a08
Kyle <piper2200s@gmail.com>
2025-03-23 23:20:28 -0500

    swapped from client to bot

--- run_bot.py
+++ run_bot.py
@@ -1,27 +1,24 @@
 import discord
+from discord.ext import commands
 import json
 from wheel.handler import handle_message
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-        
-    if message.content.startswith('$hello'):
-        await message.channel.send(f'Hello {message.author}! Is anything changing?')
-    
-    if message.content.startswith('$wheel'):
+@bot.command(name='hello')
+async def handle_hello_command(ctx):
+    print('handling hello command')
+    await ctx.send(f'Hello {ctx.author}!')
+
+
+@bot.command(name='wheel')
+async def handle_wheel_command(ctx, *, command: str):
         print('handling wheel command')
-        await handle_message(message)
+        await handle_message(ctx.message)
 
 
 def get_secret(key):
@@ -29,4 +26,4 @@ def get_secret(key):
             data = json.load(file)
             return data[key]
 
-client.run(get_secret('DISCORD_API_TOKEN'))
+bot.run(get_secret('DISCORD_API_TOKEN'))
